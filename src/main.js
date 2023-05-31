9a9250b8a6f94c4786defa3ee60c43b38d93762a
Bastián Poloni <bastian.poloni16@gmail.com>
2023-05-30 23:16:44 -0400

    first commit

---
+++ src/main.js
@@ -0,0 +1,88 @@
+class Empresa {
+    constructor(idRegistro, nombre, rut, importaciones) {
+        this.idRegistro = idRegistro;
+        this.nombre = nombre;
+        this.rut = rut; 
+        this.importaciones=[];
+    }
+    get idRegistro() {
+        return this._idRegistro;
+    }
+    set idRegistro(idReg) {
+        this._idRegistro = idReg;
+    }
+    get nombre() {
+        return this._nombre;
+    }
+    set nombre(nombre) {
+        this._nombre = nombre;
+    }
+    get rut() {
+        return this._rut;
+    }
+    set rut(rut) {
+        this._rut = rut;
+    }
+
+    get importaciones() {
+        return this._importaciones;
+    }
+
+    set importaciones(importaciones) {
+        this._importaciones = importaciones;
+    }
+
+    addImport(importacion){
+        this.importaciones.push(importacion);
+    }
+
+    sumaImportaciones(){
+        return this.importaciones.length;
+    }
+
+
+    
+}
+
+class Importacion {
+    constructor(idImportacion, producto, numProd, precioUnit) {
+        this.idImportacion = idImportacion;
+        this.producto = producto;
+        this.numProd = numProd;
+        this.precioUnit = precioUnit;
+    }
+    get idImportacion() {
+        return this._idImportacion;
+    }
+    set idImportacion(idImp) {
+        this._idImportacion = idImp;
+    }
+    get producto() {
+        return this._producto;
+    }
+    set producto(producto) {
+        this._producto = producto;
+    }
+    get numProd() {
+        return this._numProd;
+    }
+    set numProd(numProd) {
+        this._numProd = numProd;
+    }
+    get precioUnit() {
+        return this._precioUnit;
+    }
+    set precioUnit(precioUnit) {
+        this._precioUnit = precioUnit;
+    }
+}
+
+let empresa1 = new Empresa(1, 'Empresa 1', '12345678-9');
+console.log(empresa1);
+const importacion1 = new Importacion(1, 'Producto 1', 1, 100);
+const importacion2 = new Importacion(2, 'Producto 2', 2, 200);
+const importacion3 = new Importacion(3, 'Producto 3', 3, 300);
+empresa1.addImport(importacion1);
+empresa1.addImport(importacion2);
+empresa1.addImport(importacion3);
+console.log(empresa1.sumaImportaciones());
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path= require('path');
+module.exports = {
+    mode: 'development',
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+}
